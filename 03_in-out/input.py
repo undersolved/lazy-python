a62e6c56fe071fb9fb936ff3870555491a6ddfea
Undersolved <singh.bhupendra.h@gmail.com>
2026-09-11 12:41:37 +0530

    ops

--- 03_in-out/input.py
+++ 03_in-out/input.py
@@ -7,6 +7,17 @@ oh my darling I love you
 koi mujhe bole I love you
 """
 
-a = int(input("enter name"))
+# a = int(input("enter name"))
 
-print("\n Welcome", a)
+# print("\n Welcome", a)
+
+name = "Aditya Singh Hapawat"
+
+print(f"Welcome to the club{name}")
+
+# format method to add variables
+
+name2 = "Bittu"
+age2 = 34
+
+print(" Welcome to the club Mr. {}, your age is {}".format(name2, age2))
