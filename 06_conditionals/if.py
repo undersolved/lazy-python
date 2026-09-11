f25f6656e6c52115c5e930c650358ed92c2af619
Undersolved <singh.bhupendra.h@gmail.com>
2026-09-11 18:08:42 +0530

    conditionals

---
+++ 06_conditionals/if.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age"))
+
+if age >= 30:
+    print("you are old")
+elif age < 30 and age >= 15:
+    print("mera bacha hai tu bhidu")
+else:
+    print("nanha munna raahi hoon")
---
+++ 06_conditionals/nested.py
@@ -0,0 +1,11 @@
+numberx = 25
+
+if numberx > 30:
+    print("the number is greater than thirty")
+    pass
+    if numberx % 2 == 0:
+        print("it's and even number")
+    else:
+        print("it's an odd number")
+else:
+    print("the number is lesser than 30")
