32276e7df15fc803f8384ac6cc1026afe4bad177
Undersolved <singh.bhupendra.h@gmail.com>
2026-09-11 11:45:47 +0530

    variables

---
+++ 01_var_datype/variables.py
@@ -0,0 +1,19 @@
+# Variables are basically containers to strore data
+
+name = "Mohit"
+age = 20
+isStudent = True
+height = 5.9
+
+if isStudent is False:
+    print("Try again")
+else:
+    print("Welcome", name, ", your age is ", age, "and your height is", height)
+
+# Rules for variables
+
+## Variable name can't start with a numerica value
+## you should use meaningful letter
+## use underscore to combine 
+## not special characters like not is @
+## use lowercase with underscore
