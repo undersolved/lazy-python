e76ed5cf78d656fdeef665ac5637147cc820dd08
Undersolved <singh.bhupendra.h@gmail.com>
2026-09-11 21:10:31 +0530

    functions

---
+++ 13_functions/basics.py
@@ -0,0 +1,5 @@
+def addition(num1, num2):
+    return num2 + num1
+
+
+print(addition(400, 599))
---
+++ 13_functions/defparams.py
@@ -0,0 +1,6 @@
+def multiply(a=1, b=2):
+    # return a * b
+    print(a * b)
+
+
+mul1 = multiply()
