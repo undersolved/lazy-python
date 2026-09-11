020a07cab0bdfe95477ffb82dd0ebcecef009ffe
Undersolved <singh.bhupendra.h@gmail.com>
2026-09-11 22:03:12 +0530

    write-IO

---
+++ 16_write-io/append.py
@@ -0,0 +1,4 @@
+f = open("16_write-io/test.txt", "a")
+
+f.write("This line was appended")
+f.close()
---
+++ 16_write-io/basics.py
@@ -0,0 +1,5 @@
+f = open("16_write-io/test.txt", "w")
+
+f.write("ohh teriiiiiiiiiii")
+f.write("\n ohh teriiiiiiiiiii")
+f.close()
